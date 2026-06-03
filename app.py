c5e1b986e223f8ef16c578772449736051501a4f
sebastianruiz2027 <sebastian.ruizmontes@gmail.com>
2026-06-03 06:20:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from cuenta_cobro import procesar_cuenta_cobro
 from seguridad_social import combinar_ss
 from empaquetar import empaquetar
 
-app = Flask(__name__, static_folder='frontend', static_url_path='')
+app = Flask(__name__, static_folder='.', static_url_path='')
 CORS(app)
 
 @app.route('/')
